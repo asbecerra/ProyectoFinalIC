bc37aa27113599823a1ffde7ee05e4f2780634ab
“KannaW” <ansobete@gmail.com>
2018-07-01 18:31:09 -0500

    - Agregar web mi mundo escolar.

---
+++ MundoEscolar/matematicas.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MUNDO ESCOLAR</title>
+<meta name="generator" content="WYSIWYG Web Builder 11 - http://www.wysiwygwebbuilder.com">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="COLEGIO.css" rel="stylesheet">
+<link href="matematicas.css" rel="stylesheet">
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_Shape2">
+<img src="images/img0004.png" id="Shape2" alt=""></div>
+<div id="wb_Shape1">
+<img src="images/img0005.png" id="Shape1" alt=""></div>
+<div id="wb_Text1">
+<span id="wb_uid0"><strong>Inicio</strong></span></div>
+<div id="wb_Text2">
+<span id="wb_uid1"><strong>Matematicas</strong></span></div>
+<div id="wb_Text3">
+<span id="wb_uid2"><strong>Ciencias Naturales</strong></span></div>
+<div id="wb_TextArt1">
+<img src="images/img0006.png" id="TextArt1" alt="MARAVILLOSO MUNDO ESCOLAR" title="MARAVILLOSO MUNDO ESCOLAR"></div>
+<div id="wb_FontAwesomeIcon2">
+<a href="./matematicas.php"><div id="FontAwesomeIcon2"><i class="fa fa-book">&nbsp;</i></div></a></div>
+<div id="wb_Text4">
+<span id="wb_uid3"><strong>PENSUM ACADEMICO</strong></span></div>
+<div id="wb_FontAwesomeIcon4">
+<a href="./espanol.php"><div id="FontAwesomeIcon4"><i class="fa fa-paste">&nbsp;</i></div></a></div>
+<div id="wb_Text5">
+<span id="wb_uid4"><strong>Español</strong></span></div>
+<div id="wb_FontAwesomeIcon5">
+<a href="./sociales.php"><div id="FontAwesomeIcon5"><i class="fa fa-globe">&nbsp;</i></div></a></div>
+<div id="wb_Text9">
+<span id="wb_uid5"><strong>Ciencias Sociales</strong></span></div>
+<div id="wb_Image1">
+<img src="images/ipeqtuekd3.jpg" id="Image1" alt=""></div>
+<div id="wb_FontAwesomeIcon1">
+<a href="./naturales.php"><div id="FontAwesomeIcon1"><i class="fa fa-pagelines">&nbsp;</i></div></a></div>
+<div id="wb_FontAwesomeIcon3">
+<a href="./index.php"><div id="FontAwesomeIcon3"><i class="fa fa-history">&nbsp;</i></div></a></div>
+<div id="wb_Image2">
+<img src="images/c0mcn8kxn0.jpg" id="Image2" alt=""></div>
+<div id="wb_Image4">
+<img src="images/ct7d8ukjkj.jpg" id="Image4" alt=""></div>
+<div id="wb_Image5">
+<img src="images/0sv2s4a18i.jpg" id="Image5" alt=""></div>
+<div id="wb_Image3">
+<img src="images/f1452cten1.jpg" id="Image3" alt=""></div>
+<div id="wb_Text6">
+<span id="wb_uid6">Resta</span></div>
+<div id="wb_Text7">
+<span id="wb_uid7">Division</span></div>
+<div id="wb_Text8">
+<span id="wb_uid8">Suma</span></div>
+<div id="wb_Text10">
+<span id="wb_uid9">Multiplicación</span></div>
+</div>
+</body>
+</html>
