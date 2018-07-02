d6be89a03695ee2eb5f9bb1c3f0877c426e67c32
Luis Ernesto Leyva Camargo <leleyva@gmail.com>
2018-07-02 18:45:48 -0500

    LELC Upgrade Sociales

--- MundoEscolar/sociales.php
+++ MundoEscolar/sociales.php
@@ -1,9 +1,8 @@
-<!doctype html>
+﻿<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>MUNDO ESCOLAR</title>
-<meta name="generator" content="WYSIWYG Web Builder 11 - http://www.wysiwygwebbuilder.com">
 <link href="font-awesome.min.css" rel="stylesheet">
 <link href="COLEGIO.css" rel="stylesheet">
 <link href="sociales.css" rel="stylesheet">
@@ -48,11 +47,15 @@
 <div id="wb_Text6">
 <span id="wb_uid6"><strong>LAS CAPITALES</strong></span></div>
 <div id="wb_Text7">
-<span id="wb_uid7"><strong>GEOGRAFIA</strong></span></div>
+<span id="wb_uid7"><strong>GEOGRAFÍA</strong></span></div>
 <div id="wb_Image4">
 <img src="images/v640glsphu.jpg" id="Image4" alt=""></div>
 <div id="wb_Text8">
 <span id="wb_uid8"><strong>HISTORIA</strong></span></div>
+<div id="wb_Text10">
+<span id="wb_uid9"><strong>LA FAMILIA</strong></span></div>
+<div id="wb_Text11">
+<span id="wb_uid10">La palabra familia proviene del latín “familia” de “famulus”, que a su vez deriva del osco “famel” que quiere decir siervo, y más remotamente del sánscrito “vama”, que significa habitación, casa. Partiendo de esta etimología, en la antigüedad se consideraba familia al conjunto de personas y esclavos que habitaban con el señor de la casa. La familia es elconjunto de personas unidas por vínculos de matrimonio, parentesco o adopción.Considerada como una comunidad natural y universal, con base afectiva, de indiscutible formativa en el individuo y de importancia social. Se dice que es universal ya que la historia de la familia es la historia de la humanidad. En todos los grupos sociales y en todos los estadios de su civilización, siempre se ha encontrado alguna forma de organización familiar. La familia se ha modificado con el tiempo, pero siempre ha existido, por eso es un grupo social universal, el mas universal de todos.<br>Tomado de http://conceptodefinicion.de/familia/</span></div>
 </div>
 </body>
 </html>
